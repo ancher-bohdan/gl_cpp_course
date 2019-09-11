258bce5eec4c8fb81704743c1602bb6922198c03
Bohdan Chupys <chupys.bohdan@gmail.com>
2019-09-11 21:36:02 +0300

    [homework6]Initial commit

---
+++ homework6/inc/Wine.h
@@ -0,0 +1,22 @@
+#include <string>
+#include <valarray>
+#include <utility>
+
+class Wine
+{
+	private:
+		using ArrayIntT = std::valarray<int>;
+		using PairArrayT = std::pair<ArrayIntT, ArrayIntT>;
+
+		std::string name;
+		PairArrayT pair;
+	
+	public:
+		Wine() = default;
+		Wine(const char* label, int ny, const int yr[], const int bot[]);
+		Wine(const char* label, int ny);
+
+		int get_bottles(int year);
+		std::string label();
+		int sum();
+}
